c39ef18a215f79bdf70fe4f6296dbf1270812ba2
jcrangon <jc.rangon@gmail.com>
2019-10-10 17:20:11 +0200

    TTS FrontEnd fonctionnel

--- public/lib/js/script.js
+++ public/lib/js/script.js
@@ -1,3 +1,7 @@
+function FaireClignoterSousTitre (){
+    $('#clignote h5').fadeOut(900).delay(300).fadeIn(800).delay(2000);
+}
+
 jQuery(document).ready(function( $ ) {
 
     $(window).scroll(function() {
@@ -18,5 +22,99 @@ jQuery(document).ready(function( $ ) {
     $('#toTTS').fadeIn(4000);
     $('#toSTT').fadeIn(4500);
 
+    //page frontend TTS
+    if(null!==document.getElementById('api_data_voice')){
+        //requete pour récuperer la liste json des voix
+        $.ajax({
+            url: "https://gateway-lon.watsonplatform.net/text-to-speech/api/v1/voices",
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + btoa("apikey:VlX2pXSkSRzvsicpjPgqCaEfIG3DUGQYJNeOHN9XvTo1"));
+            },
+            type: 'GET',
+            dataType: 'json',
+            contentType: 'application/json',
+            processData: false,
+            data: {},
+            success: function (data) {
+                let sortable = [];
+                let finalVoiceList=[];
+                let optionList="";
+                let counter=0;
+                //creation d'un tableau de strings contenant les voix contenues dans l'objet json data.voices
+                for (const [key, value] of Object.entries(data.voices)) {
+                    //console.log(value.name);
+                    sortable.push([value.name, "Lang: "+value.language, "Gender:"+value.gender]);
+                }
+                //console.log(sortable);
+
+                //tri du tableau en fonction de la LANGUE parlée
+                sortable.sort().forEach(function(voice){
+                    let nom=voice[0].split('_')[1];
+                    finalVoiceList.push([nom,voice[0],voice[1],voice[2]])
+                });
+                //console.log(finalVoiceList);
+
+                //creation des options de la dropdown list contenant permettant la selection d'une voix et d'une langue
+                finalVoiceList.forEach(function(voice){
+                    if(counter===0){
+                        optionList+='<option value="'+voice[1]+'" selected>'+voice[0]+' - '+voice[2]+' - '+voice[3]+'</option>';
+                    }
+                    else{
+                        optionList+='<option value="'+voice[1]+'">'+voice[0]+' - '+voice[2]+' - '+voice[3]+'</option>';
+                    }
+                    counter++;
+                });
+
+                //insertion des options dans l'element select
+                $('#api_data_voice').html(optionList);
+            },
+            error: function(error){
+                console.log("Cannot get Voice data "+error);
+            }
+        });
+    }
+
+    //faire apparaitre le bouton de lecture lorsqu'il y a des caractères présents dans le textarea
+    //et le faire disparaitre dans le cas contraire.
+    $('#api_data_text').on("keyup",function(){
+        if($(this).val()===''){
+            $('#playBtn').fadeOut('fast');
+            $('#audioPlayer').slideUp('fast');
+        }
+        else{
+            $('#playBtn').fadeIn('slow');
+        }
+    });
+
+    //click sur le bouton de lecture
+    $('#playBtn').on('click', function(){
+        console.log('Play Button Clicked');
+        let text = $('#api_data_text').val();
+        let voice = $('#api_data_voice').val();
+        let url = encodeURI('https://gateway-lon.watsonplatform.net/text-to-speech/api/v1/synthesize?accept=audio/ogg;codecs=opus&download=true&text='+text+'&voice='+voice);
+        $.ajax({
+            url: url,
+            beforeSend: function(xhr) {
+                xhr.setRequestHeader("Authorization", "Basic " + btoa("apikey:VlX2pXSkSRzvsicpjPgqCaEfIG3DUGQYJNeOHN9XvTo1"));
+            },
+            type: 'GET',
+            data: {
+            },
+            xhrFields: {
+                responseType: 'blob',
+            },
+            success: function (data,status) {
+                let bloburl=window.URL.createObjectURL(data);
+                $('#audioPlayer').slideDown('3000');
+                $('#audioPlayer').attr('src', bloburl);
+            },
+            error: function(error){
+                console.log("Cannot get Voice data "+error);
+            }
+        });
+    });
+
+    //Faire clignoter le sous titre
+    setInterval('FaireClignoterSousTitre()',4000);
 
 });
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
+     * @param Request $request
      * @return RedirectResponse
      * @Route("/")
      */
-    public function redirectTTS(){
+    public function redirectTTS(Request $request){
         return $this->redirectToRoute('app_home_index');
     }
 
